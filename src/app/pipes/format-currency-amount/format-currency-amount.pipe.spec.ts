5ef1656f9319931081d7327ebf5c66a5d69dcc73
gyerosaski <gyerosaski@protonmail.com>
2026-05-13 17:28:50 +0200

    usunięcie pustych testów

--- src/app/pipes/format-currency-amount/format-currency-amount.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FormatCurrencyAmountPipe } from './format-currency-amount.pipe';
-
-describe('FormatCurrencyAmountPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FormatCurrencyAmountPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
